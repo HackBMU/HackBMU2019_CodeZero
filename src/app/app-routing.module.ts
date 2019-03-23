97c231675342fa00d5aa220975570a66e25480d8
sid12261996 <sidharthrkc@gmail.com>
2019-03-23 18:45:54 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'index',component:IndexComponent },
+  {path:'register',component:LoginComponent },
+  {path:'login',component:RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
