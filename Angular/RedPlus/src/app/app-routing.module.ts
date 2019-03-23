27a2219104d6a0a98ae3f46ca7503acd34340af6
Rishav Sood <rishavsood19@gmail.com>
2019-03-23 23:18:52 +0530

    commit

--- Angular/RedPlus/src/app/app-routing.module.ts
+++ Angular/RedPlus/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"navbar",component:NavbarComponent},
+  {path:"register",component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
